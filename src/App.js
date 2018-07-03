67e9af1ed2205a6c289ee6d5dda008e7361d3482
Chris Carr <christocarr@outlook.comgit config --global user.email christocarr@outlook.comgit config --global user.email christocarr@outlook.com>
2018-07-02 19:06:41 -0700

    Complete form to create contact and to render to contact list

--- src/App.js
+++ src/App.js
@@ -21,6 +21,14 @@ class App extends Component {
     }))
   }
 
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([ contact ])
+      }))
+    })
+  }
+
   render() {
     return (
       <div>
@@ -31,7 +39,14 @@ class App extends Component {
           />
         )}/>
 
-        <Route path='/create' component={CreateContact} />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact 
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />
       </div>
       
     )
